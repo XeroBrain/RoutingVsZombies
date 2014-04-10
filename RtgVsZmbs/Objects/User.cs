c12d1b482ecb76a0a4f26ad346c63e958c440112
Marcel Tintenklecks <mc-marcel@hotmail.de>
2014-04-10 22:05:27 +0200

    Kleiner Start, alles noch ziemlich leer, mal gucken was noch kommt

---
+++ RtgVsZmbs/Objects/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RtgVsZmbs.Objects
+{
+    public class User
+    {
+        private string Name;
+        private string Surname;
+
+        //TODO: Salt n stuff for save psw gen
+        private string Password;
+
+        //TODO: UserEnum
+        private bool IsAdmin;
+        
+        public User(string name, string surname, string password, bool usertype)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Password = password;
+            this.IsAdmin = usertype
+        }
+    }
+}
