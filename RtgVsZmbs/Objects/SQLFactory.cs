e50be34fe17bf6ca982609a07df6da1e9b78e579
MortenRoemer <morten.roemer@gmx.de>
2014-05-01 20:55:05 +0200

    SQLFactory hinzugefügt

---
+++ RtgVsZmbs/Objects/SQLFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RtgVsZmbs.Objects
+{
+    class SQLFactory
+    {
+        private SQLFactory()
+        {
+            // this is an utility class and must not be extended
+        }
+
+        public static Quizcard[] getAllQuizcards()
+        {
+            return null;
+        }
+
+        public static Quizcard getQuizcard(long id)
+        {
+            return null;
+        }
+
+        public static bool saveQuizcard(Quizcard qc)
+        {
+            return false;
+        }
+    }
+}
