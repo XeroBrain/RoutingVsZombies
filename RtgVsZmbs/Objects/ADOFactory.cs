7b3179012d5c3a877890aee03c49942d67c6c001
MortenRoemer <morten.roemer@gmx.de>
2014-05-01 22:51:19 +0200

    Als DB-Zurgiffsengine habe ich mal ADO gewählt

--- RtgVsZmbs/Objects/ADOFactory.cs
+++ RtgVsZmbs/Objects/ADOFactory.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
 
 namespace RtgVsZmbs.Objects
 {
-    class SQLFactory
+    class ADOFactory
     {
-        private SQLFactory()
+        private String mdbFilepath = "\\DB\\RvsZ_MDB.mdb";
+        private String adoProvider = "Microsoft.Jet.OLEDB.4.0";
+        private String userid = "admin";
+        private String password = "";
+
+        private ADOFactory()
         {
             // this is an utility class and must not be extended
         }
