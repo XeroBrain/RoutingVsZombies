c43854821fc6cefab923ee11f063690911da4fd5
MortenRoemer <morten.roemer@gmx.de>
2014-05-01 20:54:43 +0200

    Leere Klassen eingefügt

---
+++ RtgVsZmbs/Objects/QuizAnswer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RtgVsZmbs.Objects
+{
+    class QuizAnswer
+    {
+        private long id;
+    }
+}
--- RtgVsZmbs/Objects/Quizcard.cs
+++ RtgVsZmbs/Objects/Quizcard.cs
@@ -8,6 +8,51 @@ namespace RtgVsZmbs.Objects
 {
     class Quizcard
     {
+        private long id;
+        private String question;
+        private QuizAnswer[] answers;
 
+        private Quizcard(long id, String question, QuizAnswer[] answers)
+        {
+            this.id = id;
+            this.question = question;
+            this.answers = answers;
+        }
+
+        public long getId
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public String getQuestion
+        {
+            get
+            {
+                return question;
+            }
+        }
+
+        public QuizAnswer[] getAnswers
+        {
+            get
+            {
+                return answers;
+            }
+        }
+
+        public long answerCount
+        {
+            get
+            {
+                if (answers != null)
+                {
+                    return answers.LongLength;
+                }
+                return 0;
+            }
+        }
     }
 }
