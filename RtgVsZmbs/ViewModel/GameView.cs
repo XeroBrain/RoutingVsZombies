37452b7d8a81e6db3e36f5477907a86bc3803828
Marcel Tintenklecks <mc-marcel@hotmail.de>
2014-05-05 19:15:10 +0200

    Revert "Revert "Revert "Roll Back MainWindow"""
    This reverts commit 143267a9dd0aebc32133c76a665b68c3dad0dae2.

--- RtgVsZmbs/ViewModel/GameView.cs
+++ RtgVsZmbs/ViewModel/GameView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RtgVsZmbs.ViewModel
 {
-    class UserViewModel
+    class GameView
     {
     }
 }
--- RtgVsZmbs/ViewModel/MenueView.cs
+++ RtgVsZmbs/ViewModel/MenueView.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RtgVsZmbs.ViewModel
 {
-    class GameViewModel
+    class MenueView
     {
     }
 }
