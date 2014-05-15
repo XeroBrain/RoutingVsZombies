a62c87f718d2341afbf2079c3194b6515abfc4f1
Marcel Tintenklecks <mc-marcel@hotmail.de>
2014-05-15 16:27:37 +0200

    Neues UsersView, momentan noch sehr nackt

---
+++ RtgVsZmbs/View/UserOverviewView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RtgVsZmbs.Objects;
+
+namespace RtgVsZmbs.View
+{
+    /// <summary>
+    /// Interaction logic for UserOverviewView.xaml
+    /// </summary>
+    public partial class UserOverviewView : Window
+    {
+        public UserOverviewView()
+        {
+            InitializeComponent();
+        }
+
+        private List<User> UserList;
+    }
+}
